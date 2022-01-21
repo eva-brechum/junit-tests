338e90efc43af07b9107b834d3d418b257b64cf9
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-21 12:25:21 -0600

    finished with student class

---
+++ src/main/java/Student.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+//the Student class:
+//
+// id should be a long number used to represent a "unique user" in our application.
+//name is a String that holds the name of the student.
+//grades is an ArrayList that contains a list of Integer numbers.
+public class Student {
+   private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+
+    public Student() {
+        ArrayList<Integer>grades = new ArrayList<>();
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+        public ArrayList<Integer> getGrades() {
+            return grades;
+    }
+}
+
